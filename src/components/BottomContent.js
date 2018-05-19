4e16cbdda058d456678cefb6fb46c7e30f96b4b8
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-05-20 01:38:35 +1200

    basic portfolio layout

--- src/components/BottomContent.js
+++ src/components/BottomContent.js
@@ -3,7 +3,7 @@ import { Icon } from 'antd';
 
 function Footer(props) {
   return (
-  	<div>
+  	<div className="center">
   	<Icon type="mail" style={{ fontSize: 25 }}/><h6> Contact Me </h6>
   	<Icon type="copyright" style={{ fontSize: 25 }}/><h6> 2018 Jordan Wilson</h6>
   </div>
