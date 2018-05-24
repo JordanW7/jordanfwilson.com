5f60559b76fd0e26a8a066b7d70018c37e8ec365
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-05-24 15:10:32 +1200

    portfolio layout with Cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Button, Tag } from 'antd';
+import { Row, Col, Button, Tag, Card } from 'antd';
 import './Portfolio.css';
 
 function Portfolio(props) {
@@ -9,21 +9,30 @@ function Portfolio(props) {
       <Col span={12} className="projects_header">
         <h1>My Projects</h1>
   		</Col>
-  		<Col span={12}>
+  		<Col span={12} className="projects_header2">
   			<h2>Here's some of the things I've built</h2>
   		</Col>
   		<Col span={24}>
-  			<h4>Project 1 Title</h4>
-  		</Col>
-  		<Col span={12}>
-  		    <img className="screenshot" src="https://placeimg.com/400/300/tech" height="300" width="400" alt="Example1"/>
-      </Col>
+      <Card title="Project 1 title" extra={<Button icon="github">View Code</Button>}>
+      <Row type="flex">
       <Col span={12}>
+        <img className="screenshot" src="https://placeimg.com/400/300/tech" height="300" width="400" alt="Example1"/>
+            </Col>
+            <Col span={12}>
+            <div>
+        <Tag>ES6</Tag>
+        <Tag>React</Tag>
+        <Tag>Node.js</Tag>
+        <Tag>Express</Tag>
+        <Tag>PostgreSQL</Tag>
+      </div>
+      <br />
         <p>
           Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
         </p>
-        <Button icon="github">See the Code</Button>
-        <Button>Check out the App</Button>
+      </Col>
+      </Row>
+      </Card>
       </Col>
   	</Row>
   	</div>
