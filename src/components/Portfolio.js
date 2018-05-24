3d2573d3903d53c5c32345335a4beb56d4bfe8c0
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-05-24 14:42:29 +1200

    portfolio layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Button, Tag } from 'antd';
 import './Portfolio.css';
 
 function Portfolio(props) {
@@ -16,12 +16,14 @@ function Portfolio(props) {
   			<h4>Project 1 Title</h4>
   		</Col>
   		<Col span={12}>
-  		    <img src="" height="300" width="300" alt="Example1"/>
+  		    <img className="screenshot" src="https://placeimg.com/400/300/tech" height="300" width="400" alt="Example1"/>
       </Col>
       <Col span={12}>
         <p>
           Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
         </p>
+        <Button icon="github">See the Code</Button>
+        <Button>Check out the App</Button>
       </Col>
   	</Row>
   	</div>
