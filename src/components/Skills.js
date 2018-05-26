a29c260873fbd64227cccf7e96607be8f468f864
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-05-26 23:21:26 +1200

    skills section rework

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,45 +14,62 @@ const Skills = (props) => {
     			<span className="skills_font2" >Here's how I build.</span>
     		</Col>
         <Col span={24} className="skills_card">
-          <Row>
+          <Row type="flex" justify="middle">
             <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
-            <Icon type="desktop" style={{ fontSize: 50}} /><p>Front-End<br />Development</p>
+            <p><Icon type="desktop" style={{ fontSize: 40}} /><br />Front-End Development</p>
             </Col>
             <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
-            <span>HTML5 · CSS3 · JS · ES6 · React · Redux</span>
+            <p className="skills_list">· HTML5 · CSS3 · JS · ES6 · React · Redux</p>
             </Col>
           </Row>
         </Col>
-        </Row>
 
-        <Row gutter={16} type="flex" justify="center">
-        <Col xs={24} sm={24} md={12} lg={12} xl={12} className="skillbox">
-        <Card title={<span><Icon type="database" style={{ fontSize: 20}} /> Back-End Development</span>}>
-              Node.js · Express · PostgresSQL · JSON
-        </Card>
+        <Col span={24} className="skills_card">
+          <Row type="flex" justify="middle">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <p><Icon type="database" style={{ fontSize: 40}} /><br />Back-End Development</p>
+            </Col>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
+            <p className="skills_list">· Node.js · Express · PostgreSQL</p>
+            </Col>
+          </Row>
         </Col>
 
-        <Col xs={24} sm={24} md={12} lg={12} xl={12} className="skillbox">
-        <Card title={<span><Icon type="lock" style={{ fontSize: 20}} /> Real-time & Authentication</span>}>
-              bcrypt
-        </Card>
+        <Col span={24} className="skills_card">
+          <Row type="flex" justify="middle">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <p><Icon type="lock" style={{ fontSize: 40}} /><br />Real-time & Authentication</p>
+            </Col>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
+            <p className="skills_list">· bcrypt</p>
+            </Col>
+          </Row>
         </Col>
 
-        <Col xs={24} sm={24} md={12} lg={12} xl={12} className="skillbox">
-        <Card title={<span><Icon type="tool" style={{ fontSize: 20}} /> Development Environment</span>}>
-               Chrome DevTools · command-line · Git · npm · Heroku
-        </Card>
+        <Col span={24} className="skills_card">
+          <Row type="flex" justify="middle">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <p><Icon type="tool" style={{ fontSize: 40}} /><br />Development Environment</p>
+            </Col>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
+            <p className="skills_list">· Chrome DevTools · command-line · Git · npm · Heroku</p>
+            </Col>
+          </Row>
         </Col>
 
-        <Col xs={24} sm={24} md={12} lg={12} xl={12} className="skillbox">
-        <Card title={<span><Icon type="bulb" style={{ fontSize: 20}} /> Architecture Principles</span>}>
-              Responsive web design · Mobile-first development · Cross-browser compatibility
-        </Card>
+        <Col span={24} className="skills_card">
+          <Row type="flex" justify="middle">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <p><Icon type="bulb" style={{ fontSize: 40}} /><br />Architecture Principles</p>
+            </Col>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
+            <p className="skills_list">· Responsive web design · Mobile-first development · Cross-browser compatibility</p>
+            </Col>
+          </Row>
         </Col>
-
-        </Row>
-      </div>
-      </div>
+      </Row>
+    </div>
+  </div>
   );
 }
 
