7e6112eaffa1d3a9ea54c3469f0c4191e5d9aeb6
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-05-30 20:59:59 +1200

    tidying up - renaming Hero css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,17 +16,17 @@ const Header = (props) => {
         <p className="hero-subtitle">I'm a Creative Problem Solver and Full Stack Javascript Developer.</p>
       </Col>
         <Col span={24}>
-      		<a href='http://www.linkedin.com/in/jordanfwilson' target="_blank" rel="noopener noreferrer" className="contact_link padright">
-            <Icon type="linkedin" style={{ fontSize: 50 }}/> 
-      		  <span className="contact_text"> Linkedin</span>
+      		<a href='http://www.linkedin.com/in/jordanfwilson' target="_blank" rel="noopener noreferrer" className="contact-link">
+            <Icon type="linkedin" style={{ fontSize: 50 }} className="hero-icon"/> 
+      		  <span className="contact-text"> Linkedin</span>
           </a>
-          <a href='https://github.com/JordanW7' target="_blank" rel="noopener noreferrer" className="contact_link padright">
-            <Icon type="github" style={{ fontSize: 50 }} className="icon_pad"/>
-            <span className="contact_text"> Github</span>
+          <a href='https://github.com/JordanW7' target="_blank" rel="noopener noreferrer" className="contact-link">
+            <Icon type="github" style={{ fontSize: 50 }} className="hero-icon"/>
+            <span className="contact-text"> Github</span>
           </a>
-      		<a href='' className="contact_link">
-            <Icon type="mail" style={{ fontSize: 50 }} className="icon_pad"/>
-            <span className="contact_text"> Contact Me</span>
+      		<a href='' className="contact-link">
+            <Icon type="mail" style={{ fontSize: 50 }} className="hero-icon"/>
+            <span className="contact-text"> Contact Me</span>
           </a>
         </Col>
   	</Row>
