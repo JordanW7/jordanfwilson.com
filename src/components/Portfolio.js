4a15e0f0df0da199488d74117c5671da3e01f01f
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-01 10:19:50 +1200

    tidying portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,11 +8,11 @@ const Portfolio = (props) => {
     <div className="portfoliofull">
   	<div className="portfolio">
     	<Row type="flex">
-        <Col span={12} className="projects_header">
-          <span className="projects_font"><Icon type="appstore-o"  style={{ fontSize: "50" }} /> My Projects</span>
+        <Col span={12} className="portfolio-header">
+          <span><Icon type="appstore-o"  style={{ fontSize: "50" }} /> My Projects</span>
     		</Col>
-    		<Col span={12} className="projects_header2">
-    			<span className="projects_font2">Here's what I build.</span>
+    		<Col span={12} className="portfolio-header2">
+    			<span>Here's what I build.</span>
     		</Col>
         <ProjectList />
         </Row>
