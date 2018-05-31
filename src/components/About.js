459da20d5f783a12b126e7103221701794598494
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-01 10:31:51 +1200

    bg changes

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,11 @@ const About = (props) => {
     		</Col>
         <Col span={24} className="about_card">
         <Card>
-        Text
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a ultrices risus, ut pharetra massa. Suspendisse eleifend bibendum massa rhoncus condimentum. Integer aliquet congue urna, id ultricies lorem varius vel. Phasellus et egestas urna. Etiam interdum orci convallis tincidunt vestibulum. Aenean justo lectus, venenatis quis molestie viverra, fermentum volutpat ipsum. Morbi ac diam aliquet tellus consectetur finibus sed eget dui. Duis congue purus pellentesque ex mollis, at sollicitudin diam placerat. In hac habitasse platea dictumst.
+
+Maecenas auctor quam pulvinar, posuere tortor commodo, commodo orci. Sed ac libero ac risus varius suscipit a non dui. Nam non turpis tincidunt, hendrerit odio a, efficitur tellus. Aenean vitae lobortis metus. Fusce non iaculis nisl, non vulputate dolor. Ut dolor orci, semper sit amet ultrices in, convallis ac metus. Maecenas eget consequat eros, eget congue elit. Integer eget lectus commodo, auctor orci non, convallis massa. Nam placerat in justo sed faucibus. Phasellus nec velit dolor. Fusce sit amet lectus sit amet nulla sodales fringilla. Ut et justo a justo scelerisque ullamcorper eget vitae risus. Curabitur dignissim tempor dui eget lobortis. Donec pretium dignissim massa et rutrum. Nulla non pulvinar est. Sed aliquam, elit ac commodo tristique, ligula ante mattis neque, eget sagittis nisi dui eu nisi.
+
+Pellentesque rhoncus turpis ac lectus fringilla, ac lacinia tellus hendrerit. Nulla ultrices tortor vitae leo pharetra, id pharetra odio faucibus. Mauris laoreet ipsum vel risus aliquam tempus. Integer interdum egestas sagittis. Etiam convallis nisi eu consectetur tristique. Quisque et ullamcorper nisl, non tristique odio. Fusce posuere euismod magna, vel iaculis magna iaculis in. Donec ut nibh quis enim sagittis feugiat ut eget ex. Vestibulum malesuada tortor arcu, id sagittis leo sagittis ac. Nullam vulputate erat ultrices purus lacinia, a facilisis sem varius. Mauris iaculis neque ut ultricies egestas. Suspendisse quis ex enim. Donec non hendrerit velit, eget vestibulum tellus. Proin blandit dui sapien, in vulputate nulla maximus et. Praesent ut scelerisque neque, eu consectetur nunc. Morbi finibus tellus mi, sit amet cursus erat varius ut.
         </Card>
         </Col>
         </Row>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,63 +7,63 @@ const Skills = (props) => {
     <div className="skillsfull">
   	<div className="skills">
     	<Row type="flex">
-        <Col span={12} className="skills_header">
-          <span className="skills_font"><Icon type="code-o"  style={{ fontSize: "50" }} /> My Dev Skills</span>
+        <Col span={12} className="skills-header">
+          <span><Icon type="code-o"  style={{ fontSize: "50" }} /> My Dev Skills</span>
     		</Col>
-    		<Col span={12} className="skills_header2">
-    			<span className="skills_font2" >Here's how I build.</span>
+    		<Col span={12} className="skills-header2">
+    			<span>Here's how I build.</span>
     		</Col>
-        <Col span={24} className="skills_card">
+        <Col span={24} className="skills-card">
           <Row type="flex" justify="center" align="middle">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills-title">
             <p><Icon type="desktop" style={{ fontSize: 40}} />Front-End Development</p>
             </Col>
-            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills-details">
             <p className="skills_list">· HTML5 · CSS3 · JS · ES6 · React · Redux</p>
             </Col>
           </Row>
         </Col>
 
-        <Col span={24} className="skills_card">
+        <Col span={24} className="skills-card">
           <Row type="flex" justify="center" align="middle">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills-title">
             <p><Icon type="database" style={{ fontSize: 40}} />Back-End Development</p>
             </Col>
-            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
-            <p className="skills_list">· Node.js · Express · PostgreSQL</p>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills-details">
+            <p className="skills-list">· Node.js · Express · PostgreSQL</p>
             </Col>
           </Row>
         </Col>
 
-        <Col span={24} className="skills_card">
+        <Col span={24} className="skills-card">
           <Row type="flex" justify="center" align="middle">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills-title">
             <p><Icon type="lock" style={{ fontSize: 40}} />Real-time & Authentication</p>
             </Col>
-            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
-            <p className="skills_list">· bcrypt</p>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills-details">
+            <p className="skills-list">· bcrypt</p>
             </Col>
           </Row>
         </Col>
 
-        <Col span={24} className="skills_card">
+        <Col span={24} className="skills-card">
           <Row type="flex" justify="center" align="middle">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills-title">
             <p><Icon type="tool" style={{ fontSize: 40}} />Development Environment</p>
             </Col>
-            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
-            <p className="skills_list">· Chrome DevTools · command-line · Git · npm · Heroku</p>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills-details">
+            <p className="skills-list">· Chrome DevTools · command-line · Git · npm · Heroku</p>
             </Col>
           </Row>
         </Col>
 
-        <Col span={24} className="skills_card">
+        <Col span={24} className="skills-card">
           <Row type="flex" justify="center" align="middle">
-            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills_title">
+            <Col xs={24} sm={24} md={6} lg={6} xl={6} className="skills-title">
             <p><Icon type="bulb" style={{ fontSize: 40}} />Architecture Principles</p>
             </Col>
-            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills_details">
-            <p className="skills_list">· Responsive web design · Mobile-first development · Cross-browser compatibility</p>
+            <Col xs={24} sm={24} md={18} lg={18} xl={18} className="skills-details">
+            <p className="skills-list">· Responsive web design · Mobile-first development · Cross-browser compatibility</p>
             </Col>
           </Row>
         </Col>
