09543faa28c0eaff5892d570218f40f14a782aa6
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-02 17:57:13 +1200

    adding content

--- src/components/Project.js
+++ src/components/Project.js
@@ -20,7 +20,7 @@ const Project = ({ name, description, image, frontLink, backLink, appLink, tags 
             <Col xs={24} sm={24} md={12} lg={12} xl={12}>
               <div className="project-screenshotbox">
                 <a href={appLink} target="_blank">
-                 <img className="project-screenshot" src="https://placeimg.com/400/300/tech" height="300" width="400" alt="Example1"/>
+                 <img className="project-screenshot" src={image} alt="Example1"/>
                 </a>
               </div>
             </Col>
@@ -28,7 +28,7 @@ const Project = ({ name, description, image, frontLink, backLink, appLink, tags 
             <ProjectTags tags={tags}/>
               <br />
               <p>
-                Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+              {description}
               </p>
               </Col>
         </Row>
--- src/components/Projectlist.js
+++ src/components/Projectlist.js
@@ -3,32 +3,23 @@ import Project from './Project';
 
 const projectArray = [
 {
-  name: "Project1",
-  description: "Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-  image: "https://placeimg.com/400/300/tech",
-  frontLink: "http://google.com",
-  backLink: "",
-  appLink: "http://google.com",
+  name: "Face Recognition",
+  description: "This app detects faces in pictures using Clarifai's AI API. It also has a register/signin system and a basic profile that counts and displays user submissions.",
+  image: "/screenshots/image1.png",
+  frontLink: "https://github.com/JordanW7/face-recognition",
+  backLink: "https://github.com/JordanW7/face-recognition-backend",
+  appLink: "https://facerecognition77.herokuapp.com/",
   tags: ["ES6", "React", "Node.js", "Express", "PostgreSQL"]
 },
 {
-  name: "Project2",
-  description: "Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-  image: "https://placeimg.com/400/300/tech",
-  frontLink: "http://google.com",
-  backLink: "http://google.com",
-  appLink: "http://google.com",
-  tags: ["ES6", "React", "Node.js", "Express", "PostgreSQL"]
-},
-{
-  name: "Project3",
-  description: "Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula lorem a tristique porta. Pellentesque condimentum ac ante nec facilisis. Cras accumsan sollicitudin dui nec tincidunt. Mauris consectetur ac nibh id elementum. Sed vulputate ex id malesuada ornare. Maecenas laoreet elementum ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare ante ligula, et bibendum metus consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-  image: "https://placeimg.com/400/300/tech",
+  name: "Robo Friends",
+  description: "",
+  image: "",
   frontLink: "",
   backLink: "",
-  appLink: "http://google.com",
-  tags: ["ES6", "React", "Node.js", "Express", "PostgreSQL"]
-}
+  appLink: "",
+  tags: ["ES6", "React", "Redux"]
+},
 ]
 
 const ProjectList = () => {
