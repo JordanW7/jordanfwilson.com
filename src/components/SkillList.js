93691b2d216eaf4b579a584540d8101314a8482f
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-09 12:55:47 +1200

    skills update

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -5,7 +5,7 @@ const skillsArray = [
 {
   name: "Front-End Development",
   icon: "desktop",
-  description: "· HTML5 · CSS3 · JS · ES6 · React · Redux",
+  description: "· HTML5 · CSS3 · JS · ES6 · React · Redux · Next.js · TypeScript",
 },
 {
   name: "Back-End Development",
