23b12b7e2ebc037a9ba7c4fd800f6909485d52ef
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-16 17:00:31 +1200

    fixed minheight vh issue

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -20,7 +20,7 @@ const skillsArray = [
 {
   name: "Development Environment",
   icon: "tool",
-  description: "· Chrome DevTools · command-line · Git · npm · Webpack · Babel · ESLint · Docker · Jest · Enzyme · Prettier · CircleCI · Heroku",
+  description: "· Chrome DevTools · command-line · Git · npm · Webpack · Babel · ESLint · Docker · Jest · Enzyme · Prettier · CircleCI · AWS · Firebase · Heroku",
 },
 {
   name: "Architecture Principles",
