a265efbbe157dfd4100dc4ad67095ec154390c3c
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-07-30 21:42:24 +0700

    adding HermesCS to project list

--- src/components/Projectlist.js
+++ src/components/Projectlist.js
@@ -3,23 +3,23 @@ import Project from './Project';
 
 const projectArray = [
 {
+  name: "HermesCS",
+  description: "This app is for operating a Contact Centre. HermesCS helps manage customer info and record, manage and complete customer service requests in a fast and modern way. The app will work both in the office or out in the field on any kind of device.",
+  image: "/screenshots/hermescs.jpg",
+  frontLink: "https://github.com/JordanW7/HermesCS",
+  backLink: "https://github.com/JordanW7/HermesCS-api",
+  appLink: "",
+  tags: ["ES6", "React", "Redux", "Node.js", "Express", "PostgreSQL", "Redis", "bcrypt", "JWT", "Docker", "Prettier", "CircleCI"]
+},
+{
   name: "Face Recognition",
   description: "This app detects faces in pictures using Clarifai's AI API. It also has a register/signin system and a basic profile that counts and displays user submissions.",
-  image: "/screenshots/image1.jpg",
+  image: "/screenshots/facerecognition.jpg",
   frontLink: "https://github.com/JordanW7/face-recognition",
   backLink: "https://github.com/JordanW7/face-recognition-backend",
   appLink: "https://facerecognition77.herokuapp.com/",
   tags: ["ES6", "React", "Node.js", "Express", "PostgreSQL"]
-},
-{
-  name: "Robo Friends",
-  description: "A simple app that displays json robot friends that is searchable and uses redux for state management. Uses Jest and Enzyme for testing.",
-  image: "/screenshots/image2.jpg",
-  frontLink: "https://github.com/JordanW7/robofriends",
-  backLink: "",
-  appLink: "https://robo-friends7.herokuapp.com/",
-  tags: ["ES6", "React", "Redux"]
-},
+}
 ]
 
 const ProjectList = () => {
