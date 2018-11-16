bb64e4830d207da9101f06c7f4675e5826738fd6
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-11-16 13:57:04 +0000

    semantic HTML5

--- src/components/About.js
+++ src/components/About.js
@@ -7,30 +7,15 @@ const About = props => {
     <section className="aboutfull">
       <article className="about">
         <Row type="flex">
-          <header>
-            <Col
-              xs={24}
-              sm={24}
-              md={12}
-              lg={12}
-              xl={12}
-              className="about-header"
-            >
-              <span id="about">
-                <Icon type="message" style={{ fontSize: "50" }} /> About Me
-              </span>
-            </Col>
-            <Col
-              xs={0}
-              sm={0}
-              md={12}
-              lg={12}
-              xl={12}
-              className="about-header2"
-            >
-              <span className="about-description">Here's why I build.</span>
-            </Col>
-          </header>
+          <Col xs={24} sm={24} md={12} lg={12} xl={12} className="about-header">
+            <span id="about">
+              <Icon type="message" style={{ fontSize: "50" }} />
+              About Me
+            </span>
+          </Col>
+          <Col xs={0} sm={0} md={12} lg={12} xl={12} className="about-header2">
+            <span className="about-description">Here's why I build.</span>
+          </Col>
           <Col span={24} className="about-card">
             <p>
               <mark className="about-contents-header">#Technology</mark>
