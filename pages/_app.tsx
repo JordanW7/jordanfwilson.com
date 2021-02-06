f4b373223acfc04d87a123cb08c858abad327c30
jordanw7 <38002117+JordanW7@users.noreply.github.com>
2021-02-06 12:53:31 +0000

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ const GlobalStyle = createGlobalStyle`
   font-display: swap;
   font-weight: 300;
   font-family: 'Raleway', sans-serif;
-  scroll-behavior: smooth;
 }
 
 html,
@@ -26,21 +25,6 @@ a:active {
   color: inherit;
   text-decoration: none !important;
 }
-
-/* a:hover {
-  opacity: 1;
-  text-shadow: 0 0 5px silver;
-  transition: opacity 0.25s linear, text-shadow 0.25s linear;
-}
-
-:target:before {
-  display: block;
-  content: "";
-  height: 44px;
-  visibility: hidden;
-  pointer-events: none;
-} */
-
 `;
 
 const theme = {
